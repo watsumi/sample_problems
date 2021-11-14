9445da7af05eed8bc7fdc5635afa9d14b35c3b22
watsumi <w.utsumi2@gmail.com>
2021-11-14 20:09:12 +0900

    init

---
+++ lib/sample_problems.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sample_problems/version"
+
+module SampleProblems
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/sample_problems_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SampleProblems do
+  it "has a version number" do
+    expect(SampleProblems::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
